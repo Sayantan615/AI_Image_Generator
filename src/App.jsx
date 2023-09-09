db756ba58625fb5dcb7ea91e7970b3a67c37042f
sayantan <sayantanchowdhury615@gmail.com>
2023-09-10 00:17:11 +0530

    AI_Image_Generator Done

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import "./App.css";
+import React, { useState } from "react";
+import fetchImageDataArr from "./fetchImageData";
+
+function App() {
+  const [input, setInput] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [imagesArr, setImagesArr] = useState([]);
+  const [isLoadingvisible, setIsLoadingvisible] = useState(false);
+  const handleInput = async () => {
+    setIsLoadingvisible(true);
+    setLoading(true);
+    console.log(input);
+    const arr = await fetchImageDataArr(input);
+    setLoading(false);
+    setIsLoadingvisible(false);
+    console.log(arr);
+    setImagesArr(arr);
+  };
+  const styleObj = {
+    display: isLoadingvisible === true ? "block" : "none",
+  };
+  return (
+    <>
+      <h1>ImgGen</h1>
+      <div className="container">
+        <div className="input_wrapper">
+          <input
+            type="text"
+            id="text-input"
+            value={input}
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
+          />
+          <button id="generator-button" onClick={handleInput}>
+            Generate
+          </button>
+        </div>
+        <div className="image-grid">
+          <div className="placeholder">
+            {loading == true ? (
+              <div style={styleObj}>"Loading..."</div>
+            ) : (
+              <img src={imagesArr.length ? imagesArr[0].url : ""} alt=""></img>
+            )}
+          </div>
+          <div className="placeholder">
+            {loading == true ? (
+              <div style={styleObj}>"Loading..."</div>
+            ) : (
+              <img src={imagesArr.length ? imagesArr[1].url : ""} alt=""></img>
+            )}
+          </div>
+          <div className="placeholder">
+            {loading == true ? (
+              <div style={styleObj}>"Loading..."</div>
+            ) : (
+              <img src={imagesArr.length ? imagesArr[2].url : ""} alt=""></img>
+            )}
+          </div>
+          <div className="placeholder">
+            {loading == true ? (
+              <div style={styleObj}>"Loading..."</div>
+            ) : (
+              <img src={imagesArr.length ? imagesArr[3].url : ""} alt=""></img>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/fetchImageData.js
@@ -0,0 +1,24 @@
+async function fetchImageDataArr(prompt) {
+  const apiUrl = "https://api.openai.com/v1/images/generations";
+  const apikey = "";
+  console.log(apiUrl, apikey);
+  const requestBody = {
+    prompt,
+    n: 4,
+    size: "256x256",
+  };
+  const requestHeaders = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${apikey}`,
+  };
+  const response = await fetch(apiUrl, {
+    method: "POST",
+    body: JSON.stringify(requestBody),
+    headers: requestHeaders,
+  });
+  const data = await response.json();
+  console.log(data);
+  return data.data;
+}
+
+export default fetchImageDataArr;
